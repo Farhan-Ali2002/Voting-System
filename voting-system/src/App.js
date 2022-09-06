8b5af85182042dfd07e4bf8a01e1ebe1a198eab8
Farhan Ali <p200019@pwr.nu.edu.pk>
2022-09-06 23:42:35 +0500

    customized the login page

--- voting-system/src/App.js
+++ voting-system/src/App.js
@@ -2,12 +2,14 @@
 import {Routes,Route,Link} from "react-router-dom"
 import Login from './components/Login';
 import Practicetail from './components/practicetail';
+import SignUp from "./components/SignUp";
 function App() {
   return (
-    <div className="flex justify-center">
+    <div className="md:flex md:justify-center mb-6 bg-slate-800 h-screen">
       
       <Routes>
         <Route path='/' element = {<Login/>}></Route>
+        <Route path='/signup' element = {<SignUp/>}></Route>
       </Routes>
       
     </div>
--- voting-system/src/components/Login.js
+++ voting-system/src/components/Login.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import {Link} from "react-router-dom"
 function Login() {
     const initialValues = {
         username: "",
         password: ""
       };
       const validationSchema = Yup.object().shape({
-        username: Yup.string().min(3).max(15).required(),
+        username: Yup.string().min(3).max(15).matches(/^[A-Za-z][A-Za-z0-9_]{7,29}$/,"invalid username!").required(),
         password: Yup.string().min(4).required()
       });
       const onSubmit = () => {
@@ -15,19 +16,19 @@ function Login() {
 
       }
   return (
-    <div className="justify-center space-y-4">
-      <h2 className='text-3xl font-bold underline'>Voting System</h2>
+    <div className="mt-[100px]">
+      <h2 className='text-3xl font-bold  text-gray-50 relative left-[4rem] bottom-4'>Voting System</h2>
       <div>
         <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
         onSubmit = {onSubmit}
       >
-        <Form className="flex justify-center flex-col space-y-4 border-2 pt-1 h-80">
-          
+        <Form className='p-6 max-w-sm w-[21rem]  bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
+          <div className="flex justify-center gap-6 mb-6 flex-col ">
           <div>
-          <label>Username: </label>
-          <Field className= "border-2 h-10 w-60"
+          <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>Username: </label>
+          <Field className= "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[16rem] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             autoComplete="off"
             id="inputlog"
             name="username"
@@ -39,9 +40,9 @@ function Login() {
           
           </div>
           <div>
-          <label>Password: </label>
+          <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>Password: </label>
           <Field
-            className= "border-2 h-10 w-60"
+            className= "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[16rem] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             autoComplete="off"
             id="inputlog"
             name="password"
@@ -53,10 +54,11 @@ function Login() {
           </div>
           
           </div>
-
-          <button type="submit">Login</button>
-        
+          </div>
+          <button className ="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded relative left-[6rem] top-3" type="submit">Login</button>
+          
         </Form>
+        
 
       </Formik>
       </div>
