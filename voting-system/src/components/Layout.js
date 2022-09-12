0a47b09d0cfc2dede1fa26c3febaf65a9deeb3da
Farhan Ali <p200019@pwr.nu.edu.pk>
2022-09-12 16:23:42 +0500

    added loginpage link

--- voting-system/src/components/Layout.js
+++ voting-system/src/components/Layout.js
@@ -1,8 +1,9 @@
 import { Outlet } from "react-router-dom"
-
+import { Link } from "react-router-dom"
 const Layout = () => {
     return (
         <main className="App">
+            <Link to="/login">Login</Link>
             <Outlet />
         </main>
     )
